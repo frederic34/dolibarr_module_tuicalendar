7114cbc7af551faa34007d9264f77fc62aa9706a
Frédéric FRANCE <frederic.france@free.fr>
2020-10-24 10:06:16 +0200

    first commit

---
+++ admin/setup.php
@@ -0,0 +1,142 @@
+<?php
+/* Copyright (C) 2004-2017  Laurent Destailleur     <eldy@users.sourceforge.net>
+ * Copyright (C) 2019       Frédéric FRANCE         <frederic.france@netlogic.fr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file    tuicalendar/admin/setup.php
+ * \ingroup tuicalendar
+ * \brief   TuiCalendar setup page.
+ */
+
+// Load Dolibarr environment
+include '../config.php';
+
+// Libraries
+require_once DOL_DOCUMENT_ROOT . "/core/lib/admin.lib.php";
+require_once '../lib/tuicalendar.lib.php';
+//require_once "../class/myclass.class.php";
+
+// Translations
+$langs->loadLangs(array("admin", "tuicalendar@tuicalendar"));
+
+// Access control
+if (! $user->admin) {
+    accessforbidden();
+}
+
+// Parameters
+$action = GETPOST('action', 'alpha');
+$backtopage = GETPOST('backtopage', 'alpha');
+
+$arrayofparameters = array(
+    'TUICALENDAR_MYPARAM1' => array(
+        'css' => 'minwidth200',
+        'enabled' => 1,
+    ),
+    'TUICALENDAR_MYPARAM2' => array(
+        'css' => 'minwidth500',
+        'enabled' => 1,
+    ),
+);
+
+
+
+/*
+ * Actions
+ */
+
+if ((float) DOL_VERSION >= 6) {
+    include DOL_DOCUMENT_ROOT . '/core/actions_setmoduleoptions.inc.php';
+}
+
+
+/*
+ * View
+ */
+
+$page_name = "TuiCalendarSetup";
+llxHeader('', $langs->trans($page_name));
+
+// Subheader
+$linkback = '<a href="' . ($backtopage ? $backtopage : DOL_URL_ROOT . '/admin/modules.php?restore_lastsearch_values=1') . '">';
+$linkback .= $langs->trans("BackToModuleList") . '</a>';
+
+print load_fiche_titre($langs->trans($page_name), $linkback, 'object_tuicalendar@tuicalendar');
+
+// Configuration header
+$head = tuicalendarAdminPrepareHead();
+dol_fiche_head($head, 'settings', '', -1, "tuicalendar@tuicalendar");
+
+// Setup page goes here
+echo '<span class="opacitymedium">' . $langs->trans("TuiCalendarSetupPage") . '</span><br><br>';
+
+
+if ($action == 'edit') {
+    print '<form method="POST" action="' . $_SERVER["PHP_SELF"] . '">';
+    print '<input type="hidden" name="token" value="' . $_SESSION['newtoken'] . '">';
+    print '<input type="hidden" name="action" value="update">';
+
+    print '<table class="noborder" width="100%">';
+    print '<tr class="liste_titre"><td class="titlefield">' . $langs->trans("Parameter") . '</td>';
+    print '<td>' . $langs->trans("Value") . '</td>';
+    print '</tr>';
+
+    foreach ($arrayofparameters as $key => $val) {
+        print '<tr class="oddeven"><td>';
+        $tooltiphelp = (($langs->trans($key . 'Tooltip') != $key.'Tooltip') ? $langs->trans($key . 'Tooltip') : '');
+        print $form->textwithpicto($langs->trans($key), $tooltiphelp);
+        print '</td><td><input name="' . $key . '"  class="flat ' . (empty($val['css']) ? 'minwidth200' : $val['css']) . '" value="' . $conf->global->$key . '"></td></tr>';
+    }
+    print '</table>';
+
+    print '<br><div class="center">';
+    print '<input class="button" type="submit" value="' . $langs->trans("Save") . '">';
+    print '</div>';
+
+    print '</form>';
+    print '<br>';
+} else {
+    if (! empty($arrayofparameters)) {
+        print '<table class="noborder centpercent">';
+        print '<tr class="liste_titre">';
+        print '<td class="titlefield">' . $langs->trans("Parameter") . '</td>';
+        print '<td>' . $langs->trans("Value") . '</td>';
+        print '</tr>';
+
+        foreach ($arrayofparameters as $key => $val) {
+            print '<tr class="oddeven"><td>';
+            $tooltiphelp = (($langs->trans($key . 'Tooltip') != $key . 'Tooltip') ? $langs->trans($key . 'Tooltip') : '');
+            print $form->textwithpicto($langs->trans($key), $tooltiphelp);
+            print '</td><td>' . $conf->global->$key . '</td></tr>';
+        }
+
+        print '</table>';
+
+        print '<div class="tabsAction">';
+        print '<a class="butAction" href="' . $_SERVER["PHP_SELF"] . '?action=edit">' . $langs->trans("Modify") . '</a>';
+        print '</div>';
+    } else {
+        print '<br>' . $langs->trans("NothingToSetup");
+    }
+}
+
+
+// Page end
+dol_fiche_end();
+
+llxFooter();
+$db->close();
---
+++ css/tuicalendar.css.php
@@ -0,0 +1,377 @@
+<?php
+/* Copyright © 2019       Frédéric FRANCE <frederic.france@netlogic.fr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file    tuicalendar/css/tuicalendar.css.php
+ * \ingroup tuicalendar
+ * \brief   CSS file for module TuiCalendar.
+ */
+
+$defines = [
+    'NOREQUIRESOC',
+    'NOCSRFCHECK',
+    'NOTOKENRENEWAL',
+    'NOLOGIN',
+    'NOREQUIREHTML',
+    'NOREQUIREAJAX',
+];
+
+session_cache_limiter('public');
+// false or '' = keep cache instruction added by server
+// 'public'  = remove cache instruction added by server and if no cache-control added later,
+// a default cache delay (10800) will be added by PHP.
+
+// Load Dolibarr environment
+include '../config.php';
+
+require_once DOL_DOCUMENT_ROOT . '/core/lib/functions2.lib.php';
+
+// Load user to have $user->conf loaded (not done by default here because of NOLOGIN constant defined) and load permission if we need to use them in CSS
+/*if (empty($user->id) && ! empty($_SESSION['dol_login'])) {
+    $user->fetch('',$_SESSION['dol_login']);
+    user->getrights();
+}*/
+
+
+// Define css type
+header('Content-type: text/css');
+// Important: Following code is to cache this file to avoid page request by browser at each Dolibarr page access.
+// You can use CTRL+F5 to refresh your browser cache.
+if (empty($dolibarr_nocache)) {
+    header('Cache-Control: max-age=10800, public, must-revalidate');
+} else {
+    header('Cache-Control: no-cache');
+}
+
+?>
+.tooltip-inner {
+    max-width:240px;
+    padding:3px 8px;
+    color:#000;
+    text-align:left;
+    text-decoration:none;
+    background-color:rgb(235,235,235);
+    border-radius:5px
+}
+
+/** pour gagner un peu de place en haut */
+#id-right {
+    padding-top: 0px;
+    padding-bottom: 0px;
+}
+/**  custom bootstrap - start */
+.btn {
+  border-radius: 25px;
+  border-color: #ddd;
+}
+
+.btn:hover {
+  border: solid 1px #bbb;
+  background-color: #fff;
+}
+
+.btn:active {
+  background-color: #f9f9f9;
+  border: solid 1px #bbb;
+  outline: none;
+}
+
+.btn:disabled {
+  background-color: #f9f9f9;
+  border: solid 1px #ddd;
+  color: #bbb;
+}
+
+.btn:focus:active, .btn:focus, .btn:active {
+  outline: none;
+}
+
+.open > .dropdown-toggle.btn-default {
+  background-color: #fff;
+}
+
+<!-- .dropdown-menu {
+  top: 25px;
+  padding: 3px 0;
+  border-radius: 2px;
+  border: 1px solid #bbb;
+}
+
+.dropdown-menu > li > a {
+  padding: 9px 12px;
+  cursor: pointer;
+}
+
+.dropdown-menu > li > a:hover {
+  background-color: rgba(81, 92, 230, 0.05);
+  color: #333;
+} -->
+
+<!-- .bi15 {
+  width: 15px;
+  height: 15px;
+} -->
+/** custom fontawesome - end */
+
+.calendar-icon {
+  width: 14px;
+  height: 14px;
+}
+
+#lnb {
+  position: absolute;
+  width: 200px;
+  top: 49px;
+  bottom: 0;
+  border-right: 1px solid #d5d5d5;
+  padding: 12px 10px;
+  background: #fafafa;
+}
+
+#lnb label {
+  margin-bottom: 0;
+  cursor: pointer;
+}
+
+.lnb-new-schedule {
+  padding-bottom: 12px;
+  border-bottom: 1px solid #e5e5e5;
+}
+
+.lnb-new-schedule-btn {
+  height: 100%;
+  font-size: 12px;
+  background-color: #ff6618;
+  color: #ffffff;
+  border: 0;
+  border-radius: 25px;
+  padding: 10px 20px;
+  font-weight: bold;
+}
+
+.lnb-new-schedule-btn:hover {
+  height: 100%;
+  font-size: 12px;
+  background-color: #e55b15;
+  color: #ffffff;
+  border: 0;
+  border-radius: 25px;
+  padding: 10px 20px;
+  font-weight: bold;
+}
+
+.lnb-new-schedule-btn:active {
+  height: 100%;
+  font-size: 12px;
+  background-color: #d95614;
+  color: #ffffff;
+  border: 0;
+  border-radius: 25px;
+  padding: 10px 20px;
+  font-weight: bold;
+}
+
+.lnb-calendars > div {
+  padding: 12px 16px;
+  border-bottom: 1px solid #e5e5e5;
+  font-weight: normal;
+}
+
+.lnb-calendars-d1 {
+  padding-left: 8px;
+}
+
+.lnb-calendars-d1 label {
+  font-weight: normal;
+}
+
+.lnb-calendars-item {
+  min-height: 12px;
+  line-height: 14px;
+  padding: 6px 0;
+}
+
+.lnb-footer {
+  color: #999;
+  font-size: 11px;
+  position: absolute;
+  bottom: 12px;
+  padding-left: 16px;
+}
+
+
+#dropdownMenu-calendarType {
+  padding: 0 8px 0 11px;
+}
+
+#calendarTypeName {
+  min-width: 62px;
+  display: inline-block;
+  text-align: left;
+  line-height: 30px;
+}
+
+.move-today {
+  padding: 0 16px;
+  line-height: 30px;
+}
+
+.move-day {
+  padding: 8px;
+  font-size: 0;
+}
+
+.search-customers {
+  padding: 0 8px;
+  display: inline-block;
+  line-height: 30px;
+  vertical-align: middle;
+}
+.search-actioncode {
+  padding: 0 8px;
+  display: inline-block;
+  line-height: 30px;
+  vertical-align: middle;
+}
+.search-projects {
+  padding: 0 8px;
+  display: inline-block;
+  line-height: 30px;
+  vertical-align: middle;
+}
+
+#renderRange {
+  padding-left: 12px;
+  font-size: 18px;
+  vertical-align: middle;
+}
+
+.dropdown-menu-title .calendar-icon {
+  margin-right: 8px;
+}
+
+.calendar-bar {
+  width: 16px;
+  height: 16px;
+  margin-right: 5px;
+  display: inline-block;
+  border: 1px solid #eee;
+  vertical-align: middle;
+}
+
+.calendar-name {
+    font-size: 12px;
+    font-weight: bold;
+    vertical-align: middle;
+}
+
+.schedule-time {
+    color: #005aff;
+}
+
+/** custom fontawesome */
+.fa {
+    width: 10px;
+    height: 10px;
+    margin-right: 2px;
+}
+
+.weekday-grid-more-schedules {
+  float: right;
+  margin-top: 4px;
+  margin-right: 6px;
+  height: 18px;
+  line-height: 17px;
+  padding: 0 5px;
+  border-radius: 3px;
+  border: 1px solid #ddd;
+  font-size: 10px;
+  text-align: center;
+  color: #000;
+}
+
+.calendar-icon {
+  width: 14px;
+  height: 14px;
+  display: inline-block;
+  vertical-align: middle;
+}
+
+.calendar-font-icon {
+  font-family: 'tui-calendar-font-icon';
+  font-size: 10px;
+  font-weight: normal;
+}
+
+.img-bi {
+  background: url('../img/img-bi.png') no-repeat;
+  width: 215px;
+  height: 16px;
+}
+
+.ic_view_month {
+  background: url('../img/ic-view-month.png') no-repeat;
+}
+
+.ic_view_week {
+  background: url('../img/ic-view-week.png') no-repeat;
+}
+
+.ic_view_day {
+  background: url('../img/ic-view-day.png') no-repeat;
+}
+
+.ic-arrow-line-left {
+  background: url('../img/ic-arrow-line-left.png') no-repeat;
+}
+
+.ic-arrow-line-right {
+  background: url('../img/ic-arrow-line-right.png') no-repeat;
+}
+
+.ic-travel-time {
+  background: url('../img/ic-traveltime-w.png') no-repeat;
+}
+
+/* font icons */
+.ic-location-b:before {
+  content: '\e900';
+}
+
+.ic-lock-b:before {
+  content: '\e901';
+}
+
+.ic-milestone-b:before {
+  content: '\e902';
+}
+
+.ic-readonly-b:before {
+  content: '\e903';
+}
+
+.ic-repeat-b:before {
+  content: '\e904';
+}
+
+.ic-state-b:before {
+  content: '\e905';
+}
+
+.ic-user-b:before {
+  content: '\e906';
+}
