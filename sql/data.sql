b5c484a2b29f66786cda3af59aacc3959b4efe5e
Frédéric FRANCE <frederic.france@free.fr>
2021-02-13 09:51:26 +0100

    timezone

--- sql/data.sql
+++ sql/data.sql
@@ -13,4 +13,4 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program. If not, see http://www.gnu.org/licenses/.
 
--- VMYSQL4.3 ALTER TABLE llx_actioncomm MODIFY COLUMN tms timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+ALTER TABLE llx_actioncomm MODIFY COLUMN tms timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
