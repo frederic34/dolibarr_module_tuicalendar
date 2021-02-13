440028e31e74ef8791613b88e590ebcc7a338636
Frédéric FRANCE <frederic.france@free.fr>
2021-02-13 09:41:51 +0100

    timezone

--- core/ajax/events_ajax.php
+++ core/ajax/events_ajax.php
@@ -537,7 +537,7 @@ function getEvents($calendarId, $calendarName, $startDate, $endDate, $offset, $o
             $sql .= ")";
         }
         if ($onlylast) {
-            $sql .= " AND a.tms > '" . $db->idate($now - 120) . "'";
+            $sql .= " AND a.tms > '" . $db->idate($now - 120, 'tzserver') . "'";
         }
         // Sort on date
         $sql .= ' ORDER BY datep';
